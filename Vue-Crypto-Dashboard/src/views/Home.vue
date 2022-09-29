8eefc03074651039905856ec002f72eb5da2b101
selemondev <selemondev19@gmail.com>
2022-09-29 09:13:25 -0700

    :zap: add table

--- Vue-Crypto-Dashboard/src/views/Home.vue
+++ Vue-Crypto-Dashboard/src/views/Home.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Hero from '../components/Hero.vue';
+import Tables from "../components/Tables.vue";
 </script>
 
 <template>
     <div>
         <Hero/>
+        <Tables/>
     </div>
 </template>
