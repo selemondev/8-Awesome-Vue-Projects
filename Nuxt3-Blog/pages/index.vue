c9649efc3de995677b43ba258c4fa6c8c09b2f39
selemondev <selemondev19@gmail.com>
2022-10-07 23:35:32 -0700

    :tada: setup: nuxt-content

---
+++ Nuxt3-Blog/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+useHead({
+  title: "Nuxt 3 x Blog",
+  link: [
+    {
+      rel: "icon",
+      type: "svg",
+      href: "https://api.iconify.design/logos:nuxt-icon.svg",
+    },
+  ],
+});
+</script>
+<template>
+  <div>
+    <p class="text-red-500">Nuxt 3</p>
+  </div>
+</template>
