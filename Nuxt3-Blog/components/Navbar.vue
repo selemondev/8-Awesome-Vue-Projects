f6b376d960d3f6bf1de702dfecd9b6330c878b03
selemondev <selemondev19@gmail.com>
2022-10-08 08:17:19 -0700

    :sparkles: add NuxtLoader

--- Nuxt3-Blog/components/Navbar.vue
+++ Nuxt3-Blog/components/Navbar.vue
@@ -1,14 +1,12 @@
-<script setup lang="ts">
-</script>
 <template>
     <header>
         <nav class="w-full px-3 flex-between h-14 border-b border-gray-800">
-            <div class="flex-center space-x-2">
-                <NuxtLink to="/">
+            <NuxtLink to="/">
+                <div class="flex-center space-x-2">
                     <h1 class="text-xl md:text-2xl">Nuxt 3</h1>
-                </NuxtLink>
-                <Icon name="logos:nuxt-icon" class="text-2xl" />
-            </div>
+                    <Icon name="logos:nuxt-icon" class="text-2xl" />
+                </div>
+            </NuxtLink>
 
             <div>
                 <div>
