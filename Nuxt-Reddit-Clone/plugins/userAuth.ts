aadc8ca5f8c15f703ca79615b1159c73262242ce
selemondev <selemondev19@gmail.com>
2022-10-16 22:58:59 -0700

    :sparkles: add animation

--- Nuxt-Reddit-Clone/plugins/userAuth.ts
+++ Nuxt-Reddit-Clone/plugins/userAuth.ts
@@ -1,7 +1,6 @@
 export default defineNuxtPlugin(() => {
     addRouteMiddleware("auth", () => {
         const { $auth } = useNuxtApp();
-
         if ($auth?.currentUser?.uid) {
             return navigateTo("/")
         }
