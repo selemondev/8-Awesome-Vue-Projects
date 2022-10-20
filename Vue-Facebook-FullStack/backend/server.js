966b75186d82a74ce83a3d8fdfa1a0496745d18d
selemondev <selemondev19@gmail.com>
2022-10-20 07:36:31 -0700

    :white_check_mark: connect db

--- Vue-Facebook-FullStack/backend/server.js
+++ Vue-Facebook-FullStack/backend/server.js
@@ -3,7 +3,9 @@ const dotenv = require("dotenv").config();
 const colors = require("colors");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
 const app = express();
+connectDB();
 app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
