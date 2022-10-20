73522ac71de1b3b5f4c292f062c82a995d2baa2d
selemondev <selemondev19@gmail.com>
2022-10-20 03:14:14 -0700

    :recycle: remove project

--- Nuxt-Reddit-Clone/server/middleware/userMiddleware.ts
+++
@@ -1,8 +0,0 @@
-import type { IncomingMessage, ServerResponse } from "http";
-export default async (req: IncomingMessage, res: ServerResponse) => {
-    //@ts-ignore
-    const userCookie = useCookie(req, "userCookie");
-
-    //@ts-ignore
-    req.user = userCookie;
-}
