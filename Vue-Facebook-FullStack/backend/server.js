2c56cc57687b0fa89e0a7cb651377e8a3076f6b5
selemondev <selemondev19@gmail.com>
2022-10-22 02:44:25 -0700

    :white_check_mark: add server post route

--- Vue-Facebook-FullStack/backend/server.js
+++ Vue-Facebook-FullStack/backend/server.js
@@ -7,6 +7,7 @@ const { errorMiddleware } = require("./middlewares/errorMiddleware");
 const connectDB = require("./config/db");
 const authRoute = require("./routes/authRoute");
 const userRoute = require("./routes/userRoute");
+const postRoute = require("./routes/postRoute");
 const app = express();
 connectDB();
 app.use(cors);
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
+app.use("/api/post", postRoute);
 app.use(errorMiddleware)
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`)
