e6ba86b16919617623a109241d6c256fe0b9d952
selemondev <selemondev19@gmail.com>
2022-10-25 02:32:31 -0700

    :white_check_mark: add authentication

--- Vue-Facebook-FullStack/frontend/src/components/LoginForm.vue
+++ Vue-Facebook-FullStack/frontend/src/components/LoginForm.vue
@@ -1,36 +1,57 @@
 <script setup>
 import { Icon } from "@iconify/vue";
+import useVuelidate from "@vuelidate/core";
+import { helpers, minLength, email, required } from "@vuelidate/validators";
+import { reactive } from "vue";
+const formData = reactive({
+    email: "",
+    password: "",
+});
+
+const rules = {
+    email: { required: helpers.withMessage("Email is required", required), email },
+    password: { required: helpers.withMessage("Password is required", required), minLength: minLength(8) },
+};
+
+const v$ = useVuelidate(rules, formData);
+
+const handleSubmit = async () => {
+    const result = await v$.value.$validate();
+
+}
 </script>
 
 <template>
     <div class="grid-layout">
         <div
             class="max-w-sm w-72 mt-8 py-2 border border-gray-200 dark:border-none rounded-md sm:w-80 md:border md:border-gray-300 md:dark:border md:dark:border-gray-800 md:py-0 md:mt-20">
-            <form class="w-full bg-white dark:bg-[#0e243b] px-6 py-4 rounded-md">
+            <form class="w-full bg-white dark:bg-[#0e243b] px-6 py-4 rounded-md" @submit.prevent="handleSubmit">
                 <div class="grid-layout">
                     <Icon icon="fa6-brands:meta" class="h-10 w-10 text-[#145ceb]" />
                 </div>
 
-                <div class="pb-6">
+                <div :class="[v$.email.$error ? 'pb-0' : 'pb-6']">
                     <label for="email" class="label">Email</label>
-                    <input type="text" placeholder="Email" class="input-style">
+                    <input type="email" placeholder="Email" class="input-style" v-model="formData.email">
+                    <p class="error" v-if="v$.email.$error">{{ v$.email.$errors[0].$message }}</p>
                 </div>
 
-                <div class="pb-3">
+                <div :class="[v$.password.$error ? 'pb-0' : 'pb-3']">
                     <label for="password" class="label">Password</label>
-                    <input type="text" placeholder="Password" class="input-style">
+                    <input type="password" placeholder="Password" class="input-style" v-model="formData.password">
+                    <p class="error" v-if="v$.password.$error">{{ v$.password.$errors[0].$message }}</p>
                 </div>
 
                 <div class="py-2">
                     <p class="cursor-pointer text-sm dark:text-white md:text-base">Don't have an account yet? <span
-                            class="text-[#145ceb] font-bold hover:text-[#2f6adf] transition duration-200 ease-in">
+                            class="text-[#145ceb] font-bold hover:text-[#0042c7] dark:hover:text-[#1f6aff] transition duration-200 ease-in">
                             <router-link to="/register">Register</router-link>
                         </span></p>
                 </div>
 
                 <div>
-                    <button
-                        class="w-full py-2 px-4 bg-[#145ceb] hover:bg-[#1863f8] transition duration-200 ease-in text-white rounded-md">Sign
+                    <button type="submit"
+                        class="w-full py-2 px-4 bg-[#145ceb] hover:bg-[#1f6aff] transition duration-200 ease-in text-white rounded-md">Sign
                         In</button>
                 </div>
             </form>
--- Vue-Facebook-FullStack/frontend/src/components/Navbar.vue
+++ Vue-Facebook-FullStack/frontend/src/components/Navbar.vue
@@ -7,13 +7,19 @@ import ColorModeSwitch from "./ColorModeSwitch.vue";
         <header>
             <nav class="h-14 w-full border-b border-gray-200 dark:border-gray-800 flex-between p-2">
                 <div class="flex items-center space-x-2">
-                    <Icon icon="fa6-brands:meta" class="h-10 w-10 text-[#145ceb]"/>
+                    <router-link to="/">
+                        <Icon icon="fa6-brands:meta" class="h-10 w-10 text-[#145ceb]" />
+                    </router-link>
                     <h1 class="text-2xl font-bold text-[#145ceb] hidden dark:text-white md:flex">Meta</h1>
                 </div>
 
                 <div class="flex items-center space-x-3">
-                    <ColorModeSwitch/>
-                    <button class="py-2 px-4 text-sm rounded-md text-white bg-[#145ceb] hover:bg-[#125ef6] md:text-base">Sign In</button>
+                    <ColorModeSwitch />
+                    <router-link to="/">
+                        <button
+                            class="py-2 px-4 text-sm rounded-md text-white bg-[#145ceb] hover:bg-[#1f6aff] transition duration-200 ease-in md:text-base"> Sign In
+                        </button>
+                    </router-link>
                 </div>
             </nav>
         </header>
