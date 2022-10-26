a62859a212412abb774bdc3a47634a0383e200ee
selemondev <selemondev19@gmail.com>
2022-10-26 10:23:06 +0300

    :wrench: configure router token

--- Vue-Facebook-FullStack/frontend/src/router/index.js
+++ Vue-Facebook-FullStack/frontend/src/router/index.js
@@ -1,28 +1,68 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Login.vue";
+import { useAuthStore } from "../stores/authStore";
 const routes = [
     {
         path: "/",
         name: "login",
-        component: Login
+        component: Login,
+        meta: {
+            auth: false
+        }
     },
 
     {
         path: "/register",
         name: "register",
-        component: () => import("../views/Register.vue")
+        component: () => import("../views/Register.vue"),
+        meta: {
+            auth: false
+        }
+    },
+
+    {
+        path: "/profile",
+        name: "profile",
+        component: () => import("../views/Profile.vue"),
+        meta: {
+            auth: true
+        }
     },
 
     {
         path: "/home",
         name: "home",
-        component: () => import("../views/Home.vue")
+        component: () => import("../views/Home.vue"),
+        meta: {
+            auth: true
+        }
     }
 ];
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: routes,
+});
+
+const currentUser = () => {
+    return new Promise((resolve, reject) => {
+        const authStore = useAuthStore();
+        const token = authStore.userToken;
+        resolve(token),
+        reject
+    })
+}
+
+router.beforeEach(async (to, from, next) => {
+    if (to.matched.some((record) => record.meta.auth)) {
+        if (await currentUser()) {
+            next();
+        } else {
+            next("/");
+        }
+    } else {
+        next();
+    }
 });
 
 export default router;
