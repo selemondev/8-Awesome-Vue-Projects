a8258ef097bc55ff755e8f8eb3a38370c275c06d
selemondev <selemondev19@gmail.com>
2022-10-30 11:52:07 +0300

    :white_check_mark: add navbar

---
+++ Nuxt-3-Instagram-Clone/components/ColorModeSwitch.vue
@@ -0,0 +1,14 @@
+<script setup>
+const colorMode = useColorMode();
+console.log(colorMode.value)
+const handleClick = () => colorMode.value === "light" ? (colorMode.preference === "dark") : (colorMode.preference === "light");
+</script>
+
+<template>
+    <button @click="handleClick" class="py-2 px-2 bg-gray-200 hover:bg-gray-300/40 rounded-md">
+        <ColorScheme placeholder="...">
+            <Icon v-if="colorMode.value === 'dark'" name="ph:sun" class="w-6 h-6"/>
+            <Icon v-else name="ph:moon-light" class="w-6 h-6"/>
+        </ColorScheme>
+    </button>
+</template>
--- Nuxt-3-Instagram-Clone/nuxt.config.ts
+++ Nuxt-3-Instagram-Clone/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "nuxt-icon"],
   css: ["@/assets/main.css"],
+  colorMode: {
+    classSuffix: ''
+  },
 });
