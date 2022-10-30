bade2a8a50f8d451b8e60c94c6f52d4b60f97110
selemondev <selemondev19@gmail.com>
2022-10-30 14:46:04 +0300

    :zap: Configure firebase

--- Nuxt-3-Instagram-Clone/nuxt.config.ts
+++ Nuxt-3-Instagram-Clone/nuxt.config.ts
@@ -5,4 +5,14 @@ export default defineNuxtConfig({
   colorMode: {
     classSuffix: ""
   },
+  runtimeConfig: {
+    public: {
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID
+    }
+  }
 });
