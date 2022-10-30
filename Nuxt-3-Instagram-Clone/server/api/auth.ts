344d87923494585f149dc9b64c06baeac07caa2f
selemondev <selemondev19@gmail.com>
2022-10-30 15:51:22 +0300

    :wrench: update middlewares

--- Nuxt-3-Instagram-Clone/server/api/auth.ts
+++ Nuxt-3-Instagram-Clone/server/api/auth.ts
@@ -1,10 +1,8 @@
-import type { IncomingMessage, ServerResponse } from "http";
-export default async (req: IncomingMessage, res: ServerResponse) => {
-
-    // @ts-ignore;
-    const { user } = await useBody(req);
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event);
+    const { user } = await useBody(body);
     // @ts-ignore
     req.user = user;
 
     return { updated: true }
-}
+})
