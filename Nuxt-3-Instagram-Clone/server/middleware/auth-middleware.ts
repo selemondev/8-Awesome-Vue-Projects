e49cd941b4f45b4995d224d6144ba7ae0b32d3e1
selemondev <selemondev19@gmail.com>
2022-10-30 15:39:11 +0300

    :sparkles: add middlewares

---
+++ Nuxt-3-Instagram-Clone/server/middleware/auth-middleware.ts
@@ -0,0 +1,8 @@
+import { IncomingMessage, ServerResponse } from "http";
+
+export default async (req: IncomingMessage, res: ServerResponse) => {
+    // @ts-ignore
+    const userCookie = useCookie(req, "userCookie");
+    //@ts-ignore
+    req.user = userCookie;
+}
