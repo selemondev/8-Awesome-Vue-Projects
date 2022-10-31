410e81a87bfe39e3204f1a91243f6fc00519c118
selemondev <selemondev19@gmail.com>
2022-10-31 22:55:53 +0300

    :white_check_mark: add post component

--- Vue-Instagram-Clone/src/components/FeedComponent.vue
+++ Vue-Instagram-Clone/src/components/FeedComponent.vue
@@ -2,6 +2,23 @@
 import FeedStories from "../components/FeedStories.vue";
 import FeedInput from "../components/FeedInput.vue";
 import SuggestionsComponent from "../components/SuggestionsComponent.vue";
+import PostComponent from "../components/PostComponent.vue";
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+import { ref, watchEffect } from "vue";
+const postData = ref([]);
+watchEffect(() => {
+    const postReference = collection(db, "posts");
+    const q = query(postReference, orderBy("timestamp", "desc"));
+    const unsub = onSnapshot(q, querySnapshot => {
+        let posts = [];
+        querySnapshot.forEach(doc => {
+            posts.push({ ...doc.data(), id: doc.id })
+        });
+        postData.value = posts;
+    });
+    return () => unsub();
+})
 </script>
 <template>
     <div>
@@ -9,10 +26,14 @@ import SuggestionsComponent from "../components/SuggestionsComponent.vue";
             <section class="col-span-2">
                 <FeedStories />
                 <FeedInput class="mt-4 w-full" />
+                <div v-for="post in postData" :key="post.id">
+                    <PostComponent :id="post.id" :username="post.username" :profile="post.profile" :text="post.text"
+                        :media="post.media" :timeStamp="post.timeStamp" />
+                </div>
             </section>
             <section class="hidden xl:inline-grid md:col-span-1">
                 <main class="fixed top-16">
-                    <SuggestionsComponent/>
+                    <SuggestionsComponent />
                 </main>
             </section>
         </main>
