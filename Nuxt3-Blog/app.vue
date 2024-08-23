f8a2ea51031de40389a99954efa21b8aed3ccff2
Javon <javonpeart@gmail.com>
2024-08-23 14:30:03 -0500

    Delete Nuxt3-Blog directory

--- Nuxt3-Blog/app.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <NuxtLayout>
-      <NuxtLoadingIndicator />
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
-</template>
--- Nuxt3-Blog/nuxt.config.ts
+++
@@ -1,5 +0,0 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/content", "nuxt-icon"],
-  css: ["@/assets/main.css"],
-});
