3d65dc71259a35e435f5b5791e849636eff1704b
Javon <javonpeart@gmail.com>
2024-08-23 14:30:48 -0500

    Delete Vue-Netflix-Clone directory

--- Vue-Netflix-Clone/src/components/__tests__/Header.spec.js
+++
@@ -1,9 +0,0 @@
-import Header from "../Header.vue"
-import { render } from "@testing-library/vue";
-describe("Header", () => {
-    it("Should render a navigation button", () => {
-        const { getByText } = render(Header);
-        const button = getByText("Sign In");
-        expect(button).toBeTruthy();
-    })
-})
--- Vue-Netflix-Clone/src/supabaseConfig.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = "https://maibsiymodtnkvrzhgst.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1haWJzaXltb2R0bmt2cnpoZ3N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ1MzI1MTMsImV4cCI6MTk4MDEwODUxM30.z221LolPH7AVf_vuaj12Zvv1a-2kvPGl8kgpm9ShVvg"
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
